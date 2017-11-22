4f10737482c5b61838998544a030f4b2d7dcee09
mysteriouss <mysteriouss@126.com>
2017-11-22 14:16:40 +0800

    Update format

--- ssdb-ios/MySSDB.mm
+++ ssdb-ios/MySSDB.mm
@@ -11,24 +11,6 @@
 
 @interface MySSDB ()
 @property (nonatomic) SSDB *ssdb;
-
-#pragma mark -
-
-- (BOOL)hset:(NSString *)table key:(NSString *)key data:(NSData *)data;
-- (BOOL)hset:(NSString *)table key:(NSString *)key string:(NSString *)string;
-
-- (BOOL)hget:(NSString *)table key:(NSString *)key data:(NSData **)data;
-- (BOOL)hget:(NSString *)table key:(NSString *)key string:(NSString **)string;
-
-- (NSInteger)hsize:(NSString *)table;
-- (BOOL)hclear:(NSString *)table;
-
-- (BOOL)set:(NSString *)key json:(id)objc;
-- (BOOL)set:(NSString *)key coding:(id)objc;
-
-- (BOOL)hset:(NSString *)table key:(NSString *)key json:(id)objc;
-- (BOOL)hset:(NSString *)table key:(NSString *)key coding:(id)objc;
-
 @end
 
 @implementation MySSDB
@@ -103,50 +85,50 @@
 #pragma mark - set
 
 - (BOOL)set:(NSString *)key json:(id)objc{
-    NSData *data = [NSJSONSerialization dataWithJSONObject:objc options:kNilOptions error:nil];
-    if (data) {
-        return [self set:key data:data];
-    }
-    return NO;
+	NSData *data = [NSJSONSerialization dataWithJSONObject:objc options:kNilOptions error:nil];
+	if (data) {
+		return [self set:key data:data];
+	}
+	return NO;
 }
 - (BOOL)set:(NSString *)key coding:(id)objc{
-    if ([objc conformsToProtocol:@protocol(NSCoding)]) {
-        return [self set:key data:[NSKeyedArchiver archivedDataWithRootObject:objc]];
-    }
-    return NO;
+	if ([objc conformsToProtocol:@protocol(NSCoding)]) {
+		return [self set:key data:[NSKeyedArchiver archivedDataWithRootObject:objc]];
+	}
+	return NO;
 }
 
 - (BOOL)hset:(NSString *)table key:(NSString *)key data:(NSData *)data{
-    std::string t(table.UTF8String);
-    std::string k(key.UTF8String);
-    std::string v((const char*)data.bytes, data.length);
-    int ret = _ssdb->hset(t, k, v);
-    if(ret == 0){
-        return YES;
-    }
-    return NO;
+	std::string t(table.UTF8String);
+	std::string k(key.UTF8String);
+	std::string v((const char*)data.bytes, data.length);
+	int ret = _ssdb->hset(t, k, v);
+	if(ret == 0){
+		return YES;
+	}
+	return NO;
 }
 
 - (BOOL)hset:(NSString *)table key:(NSString *)key string:(NSString *)string{
-    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
-    return [self hset:table key:key data:data];
+	NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+	return [self hset:table key:key data:data];
 }
 
 - (BOOL)hset:(NSString *)table key:(NSString *)key json:(id)objc{
-    NSData *data = [NSJSONSerialization dataWithJSONObject:objc options:kNilOptions error:nil];
-    if (data) {
-        return [self hset:table key:key data:data];
-    }
-    return NO;
+	NSData *data = [NSJSONSerialization dataWithJSONObject:objc options:kNilOptions error:nil];
+	if (data) {
+		return [self hset:table key:key data:data];
+	}
+	return NO;
 }
 - (BOOL)hset:(NSString *)table key:(NSString *)key coding:(id)objc{
-    if ([objc conformsToProtocol:@protocol(NSCoding)]) {
-        NSData *data = [NSKeyedArchiver archivedDataWithRootObject:objc];
-        if (data) {
-            return [self hset:table key:key data:data];
-        }
-    }
-    return NO;
+	if ([objc conformsToProtocol:@protocol(NSCoding)]) {
+		NSData *data = [NSKeyedArchiver archivedDataWithRootObject:objc];
+		if (data) {
+			return [self hset:table key:key data:data];
+		}
+	}
+	return NO;
 }
 
 
@@ -154,42 +136,42 @@
 
 
 - (BOOL)hget:(NSString *)table key:(NSString *)key string:(NSString **)string{
-    NSData *data = nil;
-    BOOL ret = [self hget:table key:key data:&data];
-    if(ret && data != nil){
-        *string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    }
-    return ret;
+	NSData *data = nil;
+	BOOL ret = [self hget:table key:key data:&data];
+	if(ret && data != nil){
+		*string = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+	}
+	return ret;
 }
 
 - (BOOL)hget:(NSString *)table key:(NSString *)key data:(NSData **)data{
-    std::string t(table.UTF8String);
-    std::string k(key.UTF8String);
-    std::string v;
-    int ret = _ssdb->hget(t, k, &v);
-    if(ret == 0){
-        *data = nil;
-        return YES;
-    }else if(ret == 1){
-        *data = [NSData dataWithBytes:(const void *)v.data() length:(NSUInteger)v.size()];
-        return YES;
-    }
-    return NO;
+	std::string t(table.UTF8String);
+	std::string k(key.UTF8String);
+	std::string v;
+	int ret = _ssdb->hget(t, k, &v);
+	if(ret == 0){
+		*data = nil;
+		return YES;
+	}else if(ret == 1){
+		*data = [NSData dataWithBytes:(const void *)v.data() length:(NSUInteger)v.size()];
+		return YES;
+	}
+	return NO;
 }
 
 - (NSInteger)hsize:(NSString *)table{
-    std::string name(table.UTF8String);
-    NSInteger size = (NSUInteger)_ssdb->hsize(name);
-    return size;
+	std::string name(table.UTF8String);
+	NSInteger size = (NSUInteger)_ssdb->hsize(name);
+	return size;
 }
 
 - (BOOL)hclear:(NSString *)table{
-    std::string name(table.UTF8String);
-    int64_t ret = _ssdb->hclear(name);
-    if(ret == 0){
-        return YES;
-    }
-    return NO;
+	std::string name(table.UTF8String);
+	int64_t ret = _ssdb->hclear(name);
+	if(ret == 0){
+		return YES;
+	}
+	return NO;
 }
 
 
